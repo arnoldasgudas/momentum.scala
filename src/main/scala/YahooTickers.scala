f8d4d576cab32973c1b294fc8a38326cca4635d4
Arnoldas <arnoldasg@gmail.com>
2016-05-29 20:57:36 +0300

    passing startDate and endDate as parameters to YahooFinanceService getHistoricalQuotes method

--- src/main/scala/YahooTickers.scala
+++ src/main/scala/YahooTickers.scala
@@ -1,11 +1,19 @@
-import model.{YahooFinanceSymbol}
+import java.util.Date
+
+import model.YahooFinanceSymbol
+import org.joda.time.DateTime
 import services.YahooFinanceService
 
 object YahooTickers {
   def main(args: Array[String]) : Unit = {
 
     val service = new YahooFinanceService
-    val prices = service.getHistoricalQuotes(List(YahooFinanceSymbol.GLD, YahooFinanceSymbol.VNQ))
+
+    //todo: pass dates
+    val startDate = new DateTime("2016-04-04")
+    val endDate = new DateTime("2016-04-08")
+
+    val prices = service.getHistoricalQuotes(List(YahooFinanceSymbol.GLD, YahooFinanceSymbol.VNQ), startDate, endDate)
     println(prices)
   }
 }
--- src/main/scala/services/YahooFinanceService.scala
+++ src/main/scala/services/YahooFinanceService.scala
@@ -2,34 +2,29 @@ package services
 
 import model.YahooFinanceSymbol.YahooFinanceSymbol
 import model.{YahooFinanceHistoricalDataResponseWrapper, YahooFinanceQuote, YahooFinanceSymbol}
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
 import org.json4s.DefaultFormats
 import org.json4s.ext.EnumNameSerializer
 import org.json4s.native.JsonMethods._
-
 import scalaj.http._
 import scala.io.Source
-import scalaj.http.HttpResponse
 
 class YahooFinanceService {
-  def getHistoricalQuotes(tickers: List[YahooFinanceSymbol]): List[YahooFinanceQuote] = {
+  def getHistoricalQuotes(tickers: List[YahooFinanceSymbol], startDate: DateTime, endDate: DateTime): List[YahooFinanceQuote] = {
     implicit val formats = DefaultFormats + new EnumNameSerializer(YahooFinanceSymbol)
 
-    //todo: pass dates
-    val startDate = "2016-04-04"
-    val endDate = "2016-04-08"
-
-    val response: HttpResponse[YahooFinanceHistoricalDataResponseWrapper] = Http("http://query.yahooapis.com/v1/public/yql")
-        .param("q",
-          s"select * " +
-          s"from yahoo.finance.historicaldata " +
-          s"where symbol in (${tickers mkString("'", "','", "'")}) and startDate = '$startDate' and endDate = '$endDate'")
+    return Http("http://query.yahooapis.com/v1/public/yql")
+        .param("q", s"select * " +
+            s"from yahoo.finance.historicaldata " +
+            s"where symbol in (${tickers mkString("'", "','", "'")}) " +
+            s" and startDate = '${startDate.toString(ISODateTimeFormat.date())}' " +
+            s" and endDate = '${endDate.toString(ISODateTimeFormat.date())}'")
       .param("format", "json")
       .param("env", "store://datatables.org/alltableswithkeys")
       .param("callback", "")
       .execute(parser = { inputStream =>
         parse(Source.fromInputStream(inputStream).getLines().mkString).extract[YahooFinanceHistoricalDataResponseWrapper]
-      })
-
-    return response.body.query.results.quote
+      }).body.query.results.quote
   }
 }
